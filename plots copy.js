dbd6faa81f21cbd9731f9ca67963907db6934b46
Emily Penrose <132851315+EPenrose@users.noreply.github.com>
2023-08-19 17:00:00 -0400

    Adding updated javascript

---
+++ plots copy.js
@@ -0,0 +1,41 @@
+d3.json('http://127.0.0.1:5000/Stats').then(data => {
+  formatted_response = {};
+  avg_ppg = [];
+ for (let i=0; i<data.length; i++){
+  if(formatted_response.hasOwnProperty(data[i].POSITION)){
+    formatted_response[data[i].POSITION].push(data[i].PPG);
+  }else{
+    formatted_response[data[i].POSITION] = [data[i].PPG]
+  }
+ }
+ 
+ Object.keys(formatted_response).forEach(function(key) {
+  let sum = 0;
+  for (let i=0; i< formatted_response[key].length; i++){
+    sum += parseInt(formatted_response[key][i]);
+    //alert(formatted_response[key]);
+  }
+  alert(formatted_response[key].length);
+  alert(sum);
+  avg_ppg.push(sum/formatted_response[key].length);
+  
+});
+let trace1 = {
+  x: Object.keys(formatted_response),
+  y: avg_ppg,
+  type: 'bar'
+};
+let data_trace = [trace1];
+let layout = {
+  title: "Score by Position"
+};
+Plotly.newPlot("plot", data_trace, layout);} )
+
+
+
+
+
+
+
+
+
